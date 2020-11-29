ccca3d4aefae6e3f0bd08f1a74a8ee8c2201f690
Thiago Furlan <thiago_furlan@outlook.com>
2020-11-29 16:18:13 -0300

    Armazenamento dos dados do pedido

---
+++ src/models/pagamento.dto.ts
@@ -0,0 +1,5 @@
+export interface PagamentoDTO {
+    numerodeParcelas : number;
+    "@type" : string;
+    
+}
--- src/pages/pick-adress/pick-adress.ts
+++ src/pages/pick-adress/pick-adress.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 import { EnderecoDTO } from '../../models/endereco.dto';
+import { PedidoDTO } from '../../models/pedido.dto';
+import { CartService } from '../../services/domain/cart.service';
 import { ClienteService } from '../../services/domain/cliente.service';
 import { StorageService } from '../../services/storage_service';
 
@@ -11,13 +13,16 @@ import { StorageService } from '../../services/storage_service';
 })
 export class PickAdressPage {
 
+  pedido: PedidoDTO;
+
   items : EnderecoDTO[];
 
   constructor(
     public navCtrl: NavController, 
     public navParams: NavParams,
     public storage: StorageService,
-    public clienteService: ClienteService) {
+    public clienteService: ClienteService,
+    public cartService: CartService) {
   }
 
   ionViewDidLoad() {
@@ -27,6 +32,15 @@ export class PickAdressPage {
       this.clienteService.findByEmail(localUser.email)
         .subscribe(response => {
           this.items = response['enderecos'];
+
+          let cart = this.cartService.getCart();
+
+          this.pedido = {
+            cliente: {id: response['id']},
+            enderecoDeEntrega: null,
+            pagamento: null,
+            itens: cart.itens.map(x => {return {quantidade: x.quantidade, produto: {id: x.produto.id}}}),
+          }
         },
         error => {
           if (error.status == 403) {
@@ -37,5 +51,10 @@ export class PickAdressPage {
     else {
       this.navCtrl.setRoot('HomePage');
     }
+  }
+
+  nextPage(item: EnderecoDTO) {
+    this.pedido.enderecoDeEntrega = {id: item.id};
+    console.log(this.pedido);
   }
 }
