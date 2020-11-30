feaf6317c6fa9f42968ce27ee1da4ed51e59a9a5
Thiago Furlan <thiago_furlan@outlook.com>
2020-11-30 17:51:34 -0300

    Componente loading

--- src/pages/produtos/produtos.ts
+++ src/pages/produtos/produtos.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { IonicPage, LoadingController, NavController, NavParams } from 'ionic-angular';
 import { API_CONFIG } from '../../config/api.config';
 import { ProdutoDTO } from '../../models/produto.dto';
 import { ProdutoService } from '../../services/domain/produto.service';
@@ -16,18 +16,23 @@ export class ProdutosPage {
   constructor(
     public navCtrl: NavController, 
     public navParams: NavParams,
-    public produtoService: ProdutoService
+    public produtoService: ProdutoService,
+    public loadControl: LoadingController
     ) {
   }
 
   ionViewDidLoad() {
     let categoria_id = this.navParams.get('cat');
+    let loader = this.presentLoading();
     this.produtoService.findByCategoria(categoria_id)
       .subscribe(response => {
         this.items = response['content'];
         this.loadImageUrls();
+        loader.dismiss();
       },
-      error => {});
+      error => {
+        loader.dismiss();
+      });
   }
 
   loadImageUrls() {
@@ -44,4 +49,13 @@ export class ProdutosPage {
   callProdutoDetail(produto_id: string) {
     this.navCtrl.push('ProdutoDetailPage', {prod: produto_id});
   }
+
+  presentLoading() {
+    let loader = this.loadControl.create({
+      content: "Aguarde..."
+    });
+    loader.present();
+    return loader;
+  }
 }
+
