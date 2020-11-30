ed9028b29f22b71884e323e24fa0b4f289ef109b
Thiago Furlan <thiago_furlan@outlook.com>
2020-11-29 21:23:14 -0300

    Tela de escolha de pagamento

--- src/pages/pick-adress/pick-adress.ts
+++ src/pages/pick-adress/pick-adress.ts
@@ -55,6 +55,6 @@ export class PickAdressPage {
 
   nextPage(item: EnderecoDTO) {
     this.pedido.enderecoDeEntrega = {id: item.id};
-    console.log(this.pedido);
+    this.navCtrl.push('PaymentPage', {ped: this.pedido});
   }
 }
